208d84e4de85bce1ca36893cb232e66139f8231b
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-06-30 12:50:46 +0530

    Add files via upload

---
+++ C-Sharp OOP/Program.cs
@@ -0,0 +1,27 @@
+﻿// Class Members
+using System;
+namespace OOP
+
+{
+
+    class Car
+    {
+        string color;                 // field
+        int maxSpeed;                 // field
+        public void carMethod()    // method
+        {
+            Console.WriteLine("This is the method of Car class");
+        }
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.carMethod();  // Call the method
+        }
+    }
+
+
+}
+
+
+
