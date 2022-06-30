99e23685b1256932eda56438e3fbd339092c2bc7
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-06-30 13:07:40 +0530

    Add files via upload

---
+++ C-Sharp OOP/C-sharp Constructors/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace OOP
+
+{
+ 
+    // C-sharp constructors
+    class Car2
+    {
+        public string model;
+        public Car2()
+        {
+            model = "test model";
+        }
+
+        static void Main(string[] args)
+        {
+            Car2 obj = new Car2();
+            Console.WriteLine(obj.model);
+        }
+
+    }
+
+
+}
+
+
+
