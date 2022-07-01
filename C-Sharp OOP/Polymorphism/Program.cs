43fb839a5e2598f41a715bd1655cc9b92c1d3781
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-07-01 16:16:24 +0530

    Add files via upload

---
+++ C-Sharp OOP/Polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿// Polymorphism and overriding methods
+using System;
+namespace OOP
+
+{
+ 
+    // C-sharp inheritance
+    class Vehicle
+    {
+        public string brandName = "xyz";
+        public void honk()
+        {
+            Console.WriteLine(">>>>>>>");
+        }
+    }
+
+    class Mustang : Vehicle
+    {
+        public string modelName = "Mustang";
+        public void honk()
+        {
+            Console.WriteLine("!!!!!!!!!!");
+        }
+    }
+    class MianFunc
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Vehicle();
+            Mustang myCar = new Mustang();
+            v.honk();
+            myCar.honk();
+            Console.WriteLine(myCar.brandName + " : " + myCar.modelName);
+        }
+
+    }
+
+
+}
+
+
+
