27584e9a89e4e2ff137ffe1ab75f8efbe7057170
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-07-01 22:59:50 +0530

    Add files via upload

---
+++ C-Sharp OOP/Exception/Program.cs
@@ -0,0 +1,15 @@
+﻿// C# Exceptions
+
+using System.IO;
+try
+{
+    int[] test = { 0, 1, 2, 3, 4 };
+    Console.WriteLine(test[10]);
+
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
