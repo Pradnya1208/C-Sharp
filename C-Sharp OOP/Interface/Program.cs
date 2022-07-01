8bd4927cdddeeff4068623b859c7d159b54c0a4b
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-07-01 18:44:26 +0530

    Add files via upload

---
+++ C-Sharp OOP/Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿// C# Interface
+
+using System;
+namespace OOP
+
+{
+    interface IAnimal
+    {
+        public void interfaceMethod();//interface method does not have a body
+
+    }
+    class Dog : IAnimal
+    {
+        public void interfaceMethod()
+        {
+            Console.WriteLine("This is the definition of the interface class method");
+        }
+    }
+
+    class MianFunc
+    {
+        static void Main(string[] args)
+        {
+            //Animal anim = new Animal();// cannot create an instance of the abstract type or interface Animal
+            Dog dog = new Dog();
+            dog.interfaceMethod();
+           
+        }
+
+    }
+
+
+}
+
+
+
