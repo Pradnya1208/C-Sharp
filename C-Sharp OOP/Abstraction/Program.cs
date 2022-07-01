5d107e2786cb90c5e551b6d70939830bc2cc2a9f
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-07-01 17:12:37 +0530

    Add files via upload

---
+++ C-Sharp OOP/Abstraction/Program.cs
@@ -0,0 +1,41 @@
+﻿// Abstraction
+
+using System;
+namespace OOP
+
+{
+ 
+    abstract class Animal
+    {
+        public abstract void animalSound();
+        public void justMethod()
+        {
+            Console.WriteLine("xxxxxxxxxxx");
+
+        }
+    }
+    class Dog : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("This is the definition of the Abstract class method");
+        }
+    }
+
+    class MianFunc
+    {
+        static void Main(string[] args)
+        {
+            //Animal anim = new Animal();// cannot create an instance of the abstract type or interface Animal
+            Dog dog = new Dog();
+            dog.animalSound();
+            dog.justMethod();  
+        }
+
+    }
+
+
+}
+
+
+
