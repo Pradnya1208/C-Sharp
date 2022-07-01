9bf52933b85b306383711b26648a20d13ff70a7a
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-07-01 14:52:27 +0530

    Add files via upload

---
+++ C-Sharp OOP/Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿// C# Access Modifiers: Public , Private , Protected, Internal
+// properties and encapsulation
+using System;
+namespace OOP
+
+{
+ 
+    // C-sharp inheritance
+    class Vehicle
+    {
+        public string brandName = "xyz";
+        public void honk()
+        {
+            Console.WriteLine(">>>>>>>");
+        }
+    }
+
+    class Mustang : Vehicle
+    {
+        public string modelName = "Mustang";
+    }
+    class MianFunc
+    {
+        static void Main(string[] args)
+        {
+            Mustang myCar = new Mustang();
+            myCar.honk();
+            Console.WriteLine(myCar.brandName + " : " + myCar.modelName);
+        }
+
+    }
+
+
+}
+
+
+
