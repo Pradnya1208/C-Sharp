ad46c548863c5404b9419f918e587962dace3c3f
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-07-01 19:18:17 +0530

    Add files via upload

---
+++ C-Sharp OOP/Enums/Program.cs
@@ -0,0 +1,43 @@
+﻿// C# Enum
+
+using System;
+namespace OOP
+
+{
+    class EnumClass
+    {
+        enum Levels
+        {
+            small,
+            medium,
+            large
+        }
+
+        static void Main(string[] args)
+        {
+            Levels myElem = Levels.medium;
+            //Console.WriteLine(myElem);
+            switch(myElem)
+            {
+                case Levels.small:
+                    Console.WriteLine("small");
+                    break;
+                case Levels.medium:
+                    Console.WriteLine("medium");
+                    break ;
+                case Levels.large:
+                    Console.WriteLine("large");
+                    break;
+                default:
+                    break;
+            
+            }
+        }
+    }
+
+
+
+}
+
+
+
