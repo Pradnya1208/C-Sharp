c90fb7c0300b1673987810392d330848e00cba8f
Pradnya Rajendra Patil <64901403+Pradnya1208@users.noreply.github.com>
2022-07-01 14:00:52 +0530

    Add files via upload

---
+++ C-Sharp OOP/properties and encapsulation/Program.cs
@@ -0,0 +1,35 @@
+﻿// C# Access Modifiers: Public , Private , Protected, Internal
+// properties and encapsulation
+using System;
+namespace OOP
+
+{
+ 
+    // C-sharp constructors
+    class Car2
+    {
+        private string name; // field
+        public string Name
+        {
+            get{return name;}
+            set{name = value;}
+        }
+  
+
+    }
+    class MianFunc
+    {
+        static void Main(string[] args)
+        {
+            Car2 obj = new Car2();
+            obj.Name = "ppp";
+            Console.WriteLine(obj.Name);
+        }
+
+    }
+
+
+}
+
+
+
